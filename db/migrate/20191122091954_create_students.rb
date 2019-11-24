097a0e74232e59394b481597901ae5c8a9c28109
HoangTu <Hoang Tu>
2019-11-24 16:37:33 +0700

    ab

--- db/migrate/20191122091954_create_students.rb
+++ db/migrate/20191122091954_create_students.rb
@@ -4,8 +4,10 @@ class CreateStudents < ActiveRecord::Migration[5.2]
 
       #t.references :parent, foreign_key:true
       #t.references :lop_hoc, foreign_key:true
-      #t.references :user_role, foreign_key: true
+      t.references :user_role, foreign_key: true
+      t.references :parent, foreign_key: true
       
+
       t.timestamps
     end
   end
