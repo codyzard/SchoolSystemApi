410908ae9854ae87090a9117f4df59adbd14e532
HoangTu <Hoang Tu>
2019-11-25 21:17:04 +0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :announces
+  resources :documents
+  resources :grades
+  resources :lessons
+  resources :list_rooms
+  resources :lop_hocs
+  resources :messes
+  resources :parents
+  resources :rooms
+  resources :score_arrs
+  resources :scores
+  resources :storages
+  resources :students
+  resources :subjects
+  resources :teachers
+  resources :user_roles
+
 end
