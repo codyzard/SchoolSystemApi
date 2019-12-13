5eeb1037c224262080e668267902660ce9bea40c
HoangTu <Hoang Tu>
2019-12-13 23:02:41 +0700

    them cot lop chuyen cho LopHoc

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :lessons
   resources :lop_hocs do 
     member do
-      get 'all_student_info'
+      # get 'all_student_info'
       get 'all_student_score'
     end
   end
@@ -37,6 +37,10 @@ Rails.application.routes.draw do
   resources :user_roles
   get '/users/:authentication_token', to: 'users#show'
   put '/users/:authentication_token', to: 'users#update'
+  #classes by teacher
+  post '/lop_hocs_every_teacher', to: 'lop_hocs#getAllClassEveryTeacher'
+  post '/students_in_lophoc', to: 'lop_hocs#all_student_info'
+  post '/students_score_in_lophoc', to: 'lop_hocs#all_student_score'
   mount ActionCable.server => '/rooms'
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -73,34 +73,34 @@ Subject.create!(name: "Lịch sử")
 Subject.create!(name: "Địa lý")
 Subject.create!(name: "Ngữ văn")
 
-# 8.times do |lop|
-#     l=LopHoc.create!(grade_id: (lop/2+1),name: "#{lop/2+6}"+"/"+"#{lop/2+1}")
-#     7.times do |a|
-#         l.teachers<<Teacher.find(a+1+(lop/2)*7+1)
-#     end
-# end
-# 80.times do |n|
-#     l=LopHoc.find(n/10+1)
-#     l.students<<Student.find(n+1)
-# end
+8.times do |lop|
+    l=LopHoc.create!(grade_id: (lop/2+1),name: "#{lop/2+6}"+"/"+"#{lop/2+1}", lopChuyen: rand(0..1))
+    7.times do |a|
+        l.teachers<<Teacher.find(a+1+(lop/2)*7+1)
+    end
+end
+80.times do |n|
+    l=LopHoc.find(n/10+1)
+    l.students<<Student.find(n+1)
+end
 
-# 28.times do |teacher|
-#     teacher_id = teacher + 2
-#     sj_id = teacher_id - ( 7*(teacher/7) + 1)
-#     t = Teacher.find(teacher_id)
-#     t.subject_id = sj_id
-#     t.save
-# end
+28.times do |teacher|
+    teacher_id = teacher + 2
+    sj_id = teacher_id - ( 7*(teacher/7) + 1)
+    t = Teacher.find(teacher_id)
+    t.subject_id = sj_id
+    t.save
+end
 
-# 80.times do |student|
-#     2.times do |semester|
-#         7.times do |subject|
-#             sa=ScoreArr.create!(student_id:student+1, semester:semester+1, subject_id:subject+1, lop_hoc_id:Student.find(student+1).lop_hoc_id)
-#             sa.scores.create!(heso:1,score:rand(5..10))
-#             sa.scores.create!(heso:1,score:rand(5..10))
-#             sa.scores.create!(heso:2,score:rand(5..10))
-#             sa.scores.create!(heso:3,score:rand(5..10))
-#         end
-#     end
-# end
+80.times do |student|
+    2.times do |semester|
+        7.times do |subject|
+            sa=ScoreArr.create!(student_id:student+1, semester:semester+1, subject_id:subject+1, lop_hoc_id:Student.find(student+1).lop_hoc_id)
+            sa.scores.create!(heso:1,score:rand(5..10))
+            sa.scores.create!(heso:1,score:rand(5..10))
+            sa.scores.create!(heso:2,score:rand(5..10))
+            sa.scores.create!(heso:3,score:rand(5..10))
+        end
+    end
+end
 
